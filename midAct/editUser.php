f5b80dbf650ba60bc6f3657e9d26c88f97fcd8c6
Jib <jibrilmaristela1@gmail.com>
2025-03-12 14:20:38 +0800

    Add files via upload

---
+++ midAct/editUser.php
@@ -0,0 +1,22 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Insert Employee</h2>
+    <form action="" method="POST">
+        <input type="text" name="fname" placeholder="First Name"><br>
+        <input type="text" name="lname" placeholder="Last Name"><br>
+        <input type="text" name="department" placeholder="Department"><br>
+        <input type="text" name="salary" placeholder="Salary"><br><br>
+        <button type="submit" name="submit" class="insertButton">Insert</button>
+    </form>
+    
+</body>
+</html>
